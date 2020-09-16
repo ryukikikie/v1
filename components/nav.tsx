6eb68259979315eac64f7f631cd7edf6ac0f9b90
ryukikikie <ryukikikie@gmail.com>
2020-09-16 23:13:47 +0900

    hover & active nav lists owrking

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,26 +1,53 @@
 import Link from "next/link";
 import Logo from "./logo";
+import { useRouter } from "next/router";
 export default function Nav() {
+	const router = useRouter();
+
 	return (
 		<div>
 			<div className="nav">
 				<ul className="nav-links">
-					<li className="animated-link-wrapper">
+					<li
+						className={
+							router.pathname == "/about"
+								? "active animated-link-wrapper"
+								: "animated-link-wrapper"
+						}
+					>
 						<Link href="/about">
 							<a>About</a>
 						</Link>
 					</li>
-					<li className="animated-link-wrapper">
+					<li
+						className={
+							router.pathname == "/blog"
+								? "active animated-link-wrapper"
+								: "animated-link-wrapper"
+						}
+					>
 						<Link href="/blog">
 							<a>Blog</a>
 						</Link>
 					</li>
-					<li className="animated-link-wrapper">
+					<li
+						className={
+							router.pathname == "/portfolio"
+								? "active animated-link-wrapper"
+								: "animated-link-wrapper"
+						}
+					>
 						<Link href="/portfolio">
 							<a>Portfolio</a>
 						</Link>
 					</li>
-					<li className="animated-link-wrapper">
+					<li
+						className={
+							router.pathname == "/contact"
+								? "active animated-link-wrapper"
+								: "animated-link-wrapper"
+						}
+					>
 						<Link href="/contact">
 							<a>Contact</a>
 						</Link>
