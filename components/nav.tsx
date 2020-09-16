e514cb876e471a724b9d218dc4b1da7b36060ce5
ryukikikie <ryukikikie@gmail.com>
2020-09-16 22:42:47 +0900

    added navlink hover using ::after

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,31 +3,24 @@ import Logo from "./logo";
 export default function Nav() {
 	return (
 		<div>
-			{/* <Link href="/">
-				<a>
-					<span>
-						<Logo />
-					</span>
-				</a>
-			</Link> */}
-			<div className="nav bg-cashmere">
+			<div className="nav">
 				<ul className="nav-links">
-					<li>
+					<li className="animated-link-wrapper">
 						<Link href="/about">
 							<a>About</a>
 						</Link>
 					</li>
-					<li>
+					<li className="animated-link-wrapper">
 						<Link href="/blog">
 							<a>Blog</a>
 						</Link>
 					</li>
-					<li>
+					<li className="animated-link-wrapper">
 						<Link href="/portfolio">
 							<a>Portfolio</a>
 						</Link>
 					</li>
-					<li>
+					<li className="animated-link-wrapper">
 						<Link href="/contact">
 							<a>Contact</a>
 						</Link>
